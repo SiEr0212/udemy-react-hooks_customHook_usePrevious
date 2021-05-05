e7911a1331c30b8c1fa6e6c25e848aa755159cc4
SiEr0212 <simonertel@gmx.de>
2021-05-05 19:53:16 +0200

    added usePrevious hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import usePrevious from "./hooks/usePrevious";
 
 function App() {
+  const [age, setAge] = useState(21);
+  const previousAge = usePrevious(age)
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h2>Current Age: {age}</h2>
+        <h2>Previous Age: {previousAge}</h2>
+        <button onClick={() => setAge(age - 1)}>Make me younger</button>
       </header>
     </div>
   );
