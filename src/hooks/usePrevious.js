0307cd1615382bc3292b887f733f827313da69f0
SiEr0212 <simonertel@gmx.de>
2021-05-06 15:36:09 +0200

    added useDebugValue hook

--- src/hooks/usePrevious.js
+++ src/hooks/usePrevious.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useDebugValue } from "react";
 
 export default function usePrevious(value) {
   const ref = useRef(null);
   useEffect(() => {
     ref.current = value;
   });
+
+  useDebugValue(ref.current > 18 ? "to much" : "too little");
   return ref.current;
 }
